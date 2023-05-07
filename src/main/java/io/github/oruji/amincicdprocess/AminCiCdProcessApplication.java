6ea53d6a286f2d1cd042fdfa3810115e13c8d2ee
oruji <aorouji@payeshgaran.net>
2023-05-07 10:41:19 +0330

    Initialize application

---
+++ src/main/java/io/github/oruji/amincicdprocess/AminCiCdProcessApplication.java
@@ -0,0 +1,13 @@
+package io.github.oruji.amincicdprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AminCiCdProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AminCiCdProcessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/oruji/amincicdprocess/AminCiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.oruji.amincicdprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AminCiCdProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
